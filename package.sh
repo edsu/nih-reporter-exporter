c28985ac49be42a4a020bee3aa00fc77d054c9b4
Ed Summers <ehs@pobox.com>
2025-02-18 17:37:31 -0500

    Use environment variables for keys.

--- package.sh
+++ package.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [[ ! -f crt.pem || ! -f key.pem ]]; then
-    echo "Please put the key.pem and crt.pem files to use when signing in this directory."
+set -e
+
+SSL_KEY_FILE=$HOME/certs/key.pem
+SSL_CERT_FILE=$HOME/certs/crt.pem
+
+if [[ ! -f $SSL_KEY_FILE || ! -f $SSL_CERT_FILE ]]; then
+    echo "Please set SSL_KEY_FILE and SSL_CERT_FILE to use for signing."
     exit;
 fi
 
@@ -12,11 +17,14 @@ fi
 
 description="This Bagit package contains data that was downloaded from the NIH Reporter Exporter service https://reporter.nih.gov/exporter/ using the software at https://github.com/edsu/nih-reporter-exporter. A copy of the FAQ and data dictionary were printed as PDFs and added to the data directory."
 
-nabit archive bag \
+nabit archive nih-reporter-exporter \
     --path data \
-    --info "contact-name:Ed Summers" \
-    --info "contact-email:ehs@pobox.com" \
-    --info "external-description:${description}" \
-    --info "source-organization:" \
-    --sign crt.pem:key.pem \
+    --info "Contact-Name:Ed Summers" \
+    --info "Contact-Email:ehs@pobox.com" \
+    --info "External-Description:${description}" \
+    --sign $SSL_CERT_FILE:$SSL_KEY_FILE \
     --timestamp digicert
+
+echo "creating nih-reporter-exporter.zip"
+zip -qr nih-reporter-exporter.zip nih-reporter-exporter
+rm -rf nih-reporter-exporter
